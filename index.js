4517f1749e4032f6c31d12ade777607ebaafede7
vikas dakle <vikas@gmail.com>
2023-07-22 08:52:15 +0530

    some changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
-app.use(express.static("public"))
+// app.use(express.static("public"))
 
 app.use("/api/user", require("./routes/userRoutes"))
 app.use("/api/admin", require("./routes/adminRoutes"))
